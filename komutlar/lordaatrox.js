891cde75e69bc186fb8f0fe62e77aa0f42beb2d8
Glitch (discovered-insidious-turnip) <none>
2023-03-12 16:27:02 +0000

    🚿🕰 Checkpoint ./komutlar/lordaatrox.js:46636817/658

---
+++ komutlar/lordaatrox.js
@@ -0,0 +1,39 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+exports.run = (client, message) => {
+
+   message.channel.send('Espri yükleniyor...').then(message => {
+
+      var espriler = ['20 metrelik🤤 kılıcı⚔️ 80💪 metrelik💅 ayıya🐻‍❄️ saplamak😫😳 çok💣 zor👺 olmasa 👣 gerek💣🙉🙁'];
+
+      var espri = espriler[Math.floor(Math.random() * espriler.length)];
+
+            message.edit(`${espri}`);
+
+ });
+
+  }
+
+exports.conf = {
+
+  enabled: true,
+
+  guildOnly: false,
+
+  aliases: ['Ladam', 'lordaatrox', 'LAatrox', 'yapbi-espri'],
+
+  permLevel: 0
+
+};
+
+exports.help = {
+
+  name: 'espri',
+
+  description: 'Espri yapar.',
+
+  usage: 'espri'
+
+};
