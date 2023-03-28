2a1f5fba642392ed325ec6301a6afe3be33629d9
Glitch (discovered-insidious-turnip) <none>
2023-03-28 21:20:50 +0000

    🍎📰 Checkpoint ./komutlar/yardım.js:46636817/8809

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -1,102 +1,169 @@
-const Discord = require('discord.js')
-const ayarlar = require('../ayarlar.json') 
+const Discord = require("discord.js");
+const ayarlar = require("../ayarlar.json");
 
 exports.run = async (client, message, args, member) => {
-    var prefix = ayarlar.prefix;
-    var member = ("Selam")
-//GENEL KOMUTU
-    if(args[0] === "Genel" || args[0] === "genel" || args[0] === "General" || args[0] === "general") {
-              let Genel = new Discord.MessageEmbed()
-  .setAuthor('Genel', message.author.displayAvatarURL())
-  .setColor('#2667FF')
-  .setFooter('Bu komutu kullanan kullanıcı ' + message.author.tag, message.author.displayAvatarURL())
-   .setDescription(client.commands.filter(cmd => cmd.conf.kategori === 'Genel').map(cmd => `:white_small_square: - **${prefix}${cmd.help.name}** ${cmd.help.description}`).join("\n "))
-          return message.member.send(Genel)
-         
-       
-       return;
-    }
-    //SUNUCU KOMUTU
-      if(args[0] === "Sunucu" || args[0] === "sunucu") {
-              let Sunucu = new Discord.MessageEmbed()
-  .setAuthor('Sunucu', message.author.displayAvatarURL())
-  .setColor('#2667FF')
-  .setFooter('Bu komutu kullanan kullanıcı ' + message.author.tag, message.author.displayAvatarURL())
-   .setDescription(client.commands.filter(cmd => cmd.conf.kategori === 'Sunucu').map(cmd => `:white_small_square: - **${prefix}${cmd.help.name}** ${cmd.help.description}`).join("\n "))
-         return message.member.send(Sunucu)
-         
-      
+  var prefix = ayarlar.prefix;
+  var member = "Selam";
+  //GENEL KOMUTU
+  if (
+    args[0] === "Genel" ||
+    args[0] === "genel" ||
+    args[0] === "General" ||
+    args[0] === "general"
+  ) {
+    let Genel = new Discord.MessageEmbed()
+      .setAuthor("Genel", message.author.displayAvatarURL())
+      .setColor("#2667FF")
+      .setFooter(
+        "Bu komutu kullanan kullanıcı " + message.author.tag,
+        message.author.displayAvatarURL()
+      )
+      .setDescription(
+        client.commands
+          .filter((cmd) => cmd.conf.kategori === "Genel")
+          .map(
+            (cmd) =>
+              `:white_small_square: - **${prefix}${cmd.help.name}** ${cmd.help.description}`
+          )
+          .join("\n ")
+      );
+    return message.member.send(Genel);
 
-       return;
-    }
-//EĞLENCE KOMUTU
-  if(args[0] === "Eğlence" || args[0] === "eğlence" || args[0] === "Fun" || args[0] === "fun") {
-   let Eğlence = new Discord.MessageEmbed()
-  .setAuthor('Eğlence', message.author.displayAvatarURL())
-  .setColor('#2667FF')
-  .setFooter('Bu komutu kullanan kullanıcı ' + message.author.tag, message.author.displayAvatarURL())
-   .setDescription(client.commands.filter(cmd => cmd.conf.kategori === 'Eğlence').map(cmd => `:white_small_square: - **${prefix}${cmd.help.name}** ${cmd.help.description}`).join("\n "))
-      return message.member.send(Eğlence)
-  
-      
-           return;
+    return;
+  }
+  //SUNUCU KOMUTU
+  if (args[0] === "Sunucu" || args[0] === "sunucu") {
+    let Sunucu = new Discord.MessageEmbed()
+      .setAuthor("Sunucu", message.author.displayAvatarURL())
+      .setColor("#2667FF")
+      .setFooter(
+        "Bu komutu kullanan kullanıcı " + message.author.tag,
+        message.author.displayAvatarURL()
+      )
+      .setDescription(
+        client.commands
+          .filter((cmd) => cmd.conf.kategori === "Sunucu")
+          .map(
+            (cmd) =>
+              `:white_small_square: - **${prefix}${cmd.help.name}** ${cmd.help.description}`
+          )
+          .join("\n ")
+      );
+    return message.member.send(Sunucu);
+
+    return;
+  }
+  //EĞLENCE KOMUTU
+  if (
+    args[0] === "Eğlence" ||
+    args[0] === "eğlence" ||
+    args[0] === "Fun" ||
+    args[0] === "fun"
+  ) {
+    let Eğlence = new Discord.MessageEmbed()
+      .setAuthor("Eğlence", message.author.displayAvatarURL())
+      .setColor("#2667FF")
+      .setFooter(
+        "Bu komutu kullanan kullanıcı " + message.author.tag,
+        message.author.displayAvatarURL()
+      )
+      .setDescription(
+        client.commands
+          .filter((cmd) => cmd.conf.kategori === "Eğlence")
+          .map(
+            (cmd) =>
+              `:white_small_square: - **${prefix}${cmd.help.name}** ${cmd.help.description}`
+          )
+          .join("\n ")
+      );
+    return message.member.send(Eğlence);
+
+    return;
   }
   //MODERASYON KOMUTU
-  if(args[0] === "Moderasyon" || args[0] === "moderasyon" || args[0] === "moderation" || args[0] === "Moderation") {
-   let Moderasyon = new Discord.MessageEmbed()
-  .setAuthor('Moderasyon', message.author.displayAvatarURL())
-  .setColor('#2667FF')
-  .setFooter('Bu komutu kullanan kullanıcı ' + message.author.tag, message.author.displayAvatarURL())
-   .setDescription(client.commands.filter(cmd => cmd.conf.kategori === 'Moderasyon').map(cmd => `:white_small_square: - **${prefix}${cmd.help.name}** ${cmd.help.description}`).join("\n "))
-        return message.member.send(Moderasyon)
-             
-       
-               return;
-  }
-     //SAHİP KOMUTU
-  if(args[0] === "Sahip" || args[0] === "sahip" ) {
-    let Sahip = new Discord.MessageEmbed()
-   .setAuthor('Moderasyon', message.author.displayAvatarURL())
-   .setColor('#2667FF')
-   .setFooter('Bu komutu kullanan kullanıcı ' + message.author.tag, message.author.displayAvatarURL())
-   .setDescription(client.commands.filter(cmd => cmd.conf.kategori === 'Sahip').map(cmd => `:white_small_square: - **${prefix}${cmd.help.name}** ${cmd.help.description}`).join("\n "))
-    return message.member.send(Sahip)
-              
-        
-                return;
-   }
+  if (
+    args[0] === "Moderasyon" ||
+    args[0] === "moderasyon" ||
+    args[0] === "moderation" ||
+    args[0] === "Moderation"
+  ) {
+    let Moderasyon = new Discord.MessageEmbed()
+      .setAuthor("Moderasyon", message.author.displayAvatarURL())
+      .setColor("#2667FF")
+      .setFooter(
+        "Bu komutu kullanan kullanıcı " + message.author.tag,
+        message.author.displayAvatarURL()
+      )
+      .setDescription(
+        client.commands
+          .filter((cmd) => cmd.conf.kategori === "Moderasyon")
+          .map(
+            (cmd) =>
+              `:white_small_square: - **${prefix}${cmd.help.name}** ${cmd.help.description}`
+          )
+          .join("\n ")
+      );
+    return message.member.send(Moderasyon);
 
-//YARDIM KOMUTU
-  
+    return;
+  }
+  //SAHİP KOMUTU
+  if (args[0] === "Sahip" || args[0] === "sahip") {
+    let Sahip = new Discord.MessageEmbed()
+      .setAuthor("Moderasyon", message.author.displayAvatarURL())
+      .setColor("#2667FF")
+      .setFooter(
+        "Bu komutu kullanan kullanıcı " + message.author.tag,
+        message.author.displayAvatarURL()
+      )
+      .setDescription(
+        client.commands
+          .filter((cmd) => cmd.conf.kategori === "Sahip")
+          .map(
+            (cmd) =>
+              `:white_small_square: - **${prefix}${cmd.help.name}** ${cmd.help.description}`
+          )
+          .join("\n ")
+      );
+    return message.member.send(Sahip);
+
+    return;
+  }
+
+  //YARDIM KOMUTU
+
   let embed = new Discord.MessageEmbed()
-  .setAuthor('Yardım Komutları', message.author.displayAvatarURL())
-  .setThumbnail(client.user.avatarURL())
-  .setColor('#FFFB05')
-  .setDescription(`**Örnek Kullanım:** \`${prefix}yardım Kategori\` \n **Örnek:** \`${prefix}yardım Genel\``)
-  .addField('Kategoriler:', `
+    .setAuthor("Yardım Komutları", message.author.displayAvatarURL())
+    .setThumbnail(client.user.avatarURL())
+    .setColor("#FFFB05")
+    .setDescription(
+      `**Örnek Kullanım:** \`${prefix}yardım Kategori\` \n **Örnek:** \`${prefix}yardım Genel\``
+    )
+    .addField(
+      "Kategoriler:",
+      `
   **[${prefix}yardım Genel]** 
   **[${prefix}yardım Eğlence]**
   **[${prefix}yardım Moderasyon]**
   **[${prefix}yardım Sunucu]**
   **[${prefix}yardım Sahip]**
-  `)
- .setFooter('Bu komutu kullanan kullanıcı ' + message.author.tag, message.author.displayAvatarURL())
-  message.member.send(embed)
+  `
+    )
+    .setFooter(
+      "Bu komutu kullanan kullanıcı " + message.author.tag,
+      message.author.displayAvatarURL()
+    );
+  message.member.send(embed);
+};
 
-  
-}
-  
+exports.conf = {
+  aliases: ["help", "cmds", "komutlar", "y"],
+  permLevel: 0,
+  kategori: "Genel",
+};
 
-
-  exports.conf = {
-    aliases: ['help', 'cmds', 'komutlar','y'], 
-    permLevel: 0, 
-    kategori: "Genel"
-
-  };
-
-  exports.help = {
-    name: 'yardım',  
-    description: 'Komutlar hakkında bilgi verir.',
-    usage: 'yardım', 
-  };
+exports.help = {
+  name: "yardım",
+  description: "Komutlar hakkında bilgi verir.",
+  usage: "yardım",
+};
